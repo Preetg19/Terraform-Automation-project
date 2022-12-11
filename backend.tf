19057dc89aa82c46dc055a8fb61aead9f90d86da
Preetg19 <gprateek19@gmail.com>
2022-12-11 22:03:20 +0530

    changed files

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-2"
+    bucket = "my-tf-store-bucket"
     key = "main"
     region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
